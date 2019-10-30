87f78d544135298d77dab6f68c2937db41dade8c
Jack Barry <jack.barry@flashparking.com>
2019-10-30 11:55:31 -0500

    Rebuild

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Excecutes a command and resolves with the response of the command
+ *
+ * @param command The command you wish to execute
+ * @returns A Promise resolving to a string representing the output from the
+ *  command
+ */
+export declare const promiseExec: (command: string) => Promise<string>;
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,21 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var child_process_1 = require("child_process");
+/**
+ * Excecutes a command and resolves with the response of the command
+ *
+ * @param command The command you wish to execute
+ * @returns A Promise resolving to a string representing the output from the
+ *  command
+ */
+exports.promiseExec = function (command) {
+    return new Promise(function (res, rej) {
+        child_process_1.exec(command, function (err, stdout, stderr) {
+            if (err)
+                rej(err);
+            if (stderr)
+                rej(stderr);
+            res(stdout);
+        });
+    });
+};
